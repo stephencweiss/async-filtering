28b712fa4d43ceb75189b6e019bac75a12e0ef35
Stephen <stephen.c.weiss@gmail.com>
2022-01-30 11:42:16 -0600

    ✨ Asynchronous filtering

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ const postsPath = path.join(__dirname);
 async function readPosts() {
   const filter = async (filePath) => {
     const fullPath = path.join(postsPath, filePath);
-    const isFile = (await fs.stat(fullPath)).isFile();
-    console.log({ isFile });
-    return await isFile;
+    return (await fs.stat(fullPath)).isFile();
   };
 
   const dir = await fs.readdir(postsPath);
-  const filtered = await dir.filter(filter);
+  const filtered = await dir.reduce(
+    async (acc, cur) => ((await filter(cur)) ? [...(await acc), cur] : acc),
+    []
+  );
   return await filtered;
 }
 
-readPosts().then((res) => console.assert(res.length === 3));
+readPosts().then((res) => {
+  console.log({ res });
+  console.assert(res.length === 4);
+});
